5bd460f9b4efa39e490bb7e55d0a2265ed468376
AdeleNikitina <57428032+AdeleNikitina@users.noreply.github.com>
2020-04-26 00:31:27 +0500

    Лекция 1.7

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,14 +3,37 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		// Лекция 1.5. Повторяем урок
-		String somebody = "world";
-		System.out.println("Hello, " +  somebody + "!");
+		// Лекция 1.7. Повторяем урок
+		hello("world");
+		hello("Adele");
+		hello("Alexei");
+
+		// Лекция 1.7. Повторяем урок
+		double l = 5;
+		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+		double a = 4;
+		double b = 5;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
 
 		// Лекция 1.5. Повторяем урок
-		double l = 8;
-		double s = l * l;
-		System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+		//double l = 8;
+		//double s = l * l;
+		//System.out.println("Площадь квадрата со стороной " + l + " = " + s);
 	}
-	
+
+	// Лекция 1.7. Повторяем урок
+	public static void hello(String somebody) {
+		System.out.println("Hello, " +  somebody + "!");
+	}
+
+	// Лекция 1.7. Повторяем урок
+	public static double area(double len){
+		return  len * len;
+	}
+
+	// Лекция 1.7. Повторяем урок
+	public static double area(double a, double b){
+		return  a * b;
+	}
 }
