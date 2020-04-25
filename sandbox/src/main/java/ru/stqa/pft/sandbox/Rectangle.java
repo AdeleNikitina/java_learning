1e332ee8b2974bae6557e4ae8a67108fee68eb5f
AdeleNikitina <57428032+AdeleNikitina@users.noreply.github.com>
2020-04-26 00:57:23 +0500

    Лекция 1.8

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -0,0 +1,12 @@
+// Лекция 1.8. Повторяем урок
+package ru.stqa.pft.sandbox;
+
+public class Rectangle {
+  public double a;
+  public double b;
+
+  public Rectangle(double a, double b) {
+    this.a = a;
+    this.b = b;
+  }
+}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,11 @@
+// Лекция 1.8. Повторяем урок
+package ru.stqa.pft.sandbox;
+
+public class Square {
+
+  public double l;
+
+  public  Square(double l) {
+    this.l = l;
+  }
+}
