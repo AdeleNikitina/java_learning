e7c919411366646170d315795c16c21c28eaa8de
AdeleNikitina <57428032+AdeleNikitina@users.noreply.github.com>
2020-05-03 00:15:38 +0500

    Задание №4

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/PersonCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/PersonCreationTest.java
@@ -6,8 +6,8 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -15,7 +15,7 @@ import java.util.concurrent.TimeUnit;
 public class PersonCreationTest {
   private WebDriver webDriver;
 
-  @BeforeClass(alwaysRun = true)
+  @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
     webDriver = new FirefoxDriver();
     webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -89,7 +89,7 @@ public class PersonCreationTest {
     webDriver.findElement(By.linkText("Logout")).click();
   }
 
-  @AfterClass(alwaysRun = true)
+  @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
     webDriver.quit();
   }
