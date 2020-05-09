e61881ef227caa7fe829e232eb953957db8fadca
AdeleNikitina <57428032+AdeleNikitina@users.noreply.github.com>
2020-05-09 20:19:13 +0500

    Изменены названия методов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/PersonDeletionTestsFromEdit.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/PersonDeletionTestsFromEdit.java
@@ -3,12 +3,12 @@ package ru.stqa.pft.addressbook.tests;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
-public class PersonDeletionTests_1 extends TestBase {
+public class PersonDeletionTestsFromEdit extends TestBase {
   private WebDriver webDriver;
 
   // Удаление через страницу редактирования контакта
   @Test
-  public void testPersonDeletion_1() throws Exception {
+  public void testPersonDeletionFromEdit() throws Exception {
     app.getPersonHelper().editPerson();
     app.getPersonHelper().deletePerson();
     app.getNavigationHelper().goToHomePageFromMenu();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/PersonDeletionTestsFromMenu.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/PersonDeletionTestsFromMenu.java
@@ -3,12 +3,12 @@ package ru.stqa.pft.addressbook.tests;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
-public class PersonDeletionTests_2 extends TestBase {
+public class PersonDeletionTestsFromMenu extends TestBase {
   private WebDriver webDriver;
 
   // Удаление через главную страницу
   @Test
-  public void testPersonDeletion_2() throws Exception {
+  public void testPersonDeletionFromMenu() throws Exception {
     app.getPersonHelper().selectContact();
     app.getPersonHelper().deletePerson();
     app.getPersonHelper().acceptDeletionContact();
