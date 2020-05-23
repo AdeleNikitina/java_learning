f3dc92c8cbda2a81d0ae66f737dffdc775014d6f
AdeleNikitina <57428032+AdeleNikitina@users.noreply.github.com>
2020-05-23 23:54:05 +0500

    Лекция 4.4. Выбор элемента списка по порядковому номеру

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -28,8 +28,8 @@ public class GroupHelper extends HelperBase {
   }
 
   // Выбрать группу
-  public void selectGroup() {
-    click(By.name("selected[]"));
+  public void selectGroup(int index) {
+    webDriver.findElements(By.name("selected[]")).get(index).click();
   }
 
   // Удалить выбранную группу
