f58c690a4c05545bf1782cea64899be8c3dcea69
AdeleNikitina <57428032+AdeleNikitina@users.noreply.github.com>
2020-05-31 23:32:53 +0500

    Лекция 5.6. Hamcrest: улучшение внешнего вида проверок. Groups

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -4,10 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class GroupHelper extends HelperBase {
 
@@ -89,8 +88,8 @@ public class GroupHelper extends HelperBase {
     return webDriver.findElements(By.name("selected[]")).size();
   }
 
-  public Set<GroupData> all() {
-    Set<GroupData> groups = new HashSet<GroupData>();
+  public Groups all() {
+    Groups groups = new Groups();
     List<WebElement> elements = webDriver.findElements(By.cssSelector("span.group"));
     for (WebElement element : elements) {
       String name = element.getText();
