dc4ed8f6536297a0e0cc74ae1a259102cb9d22c7
AdeleNikitina <57428032+AdeleNikitina@users.noreply.github.com>
2020-06-11 21:28:30 +0500

    Лекция 6.11. Протоколирование (лог-файлы)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,11 +1,20 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManger;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static final ApplicationManger app
           = new ApplicationManger(System.getProperty("browser", BrowserType.FIREFOX));
@@ -19,6 +28,16 @@ public class TestBase {
   public void tearDown() throws Exception {
     app.getSessionHelper().logout();
     app.stop();
+  }
+
+  @BeforeMethod (alwaysRun = true)
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Start test " + m.getName() + "with parameters" + Arrays.asList(p));
+  }
+
+  @AfterMethod (alwaysRun = true)
+  public void logTestStop(Method m, Object[] p) {
+    logger.info("Stop test " + m.getName() + "with parameters" + Arrays.asList(p));
   }
 
 }
