e9b2007353f396be5abeb1a6e0c6b8bd417b593c
AdeleNikitina <57428032+AdeleNikitina@users.noreply.github.com>
2020-06-16 21:07:12 +0500

    Лекция 9.6. REST: взаимодействие с GitHub

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("f1a33887a254b54629d86093c27f09cc0a33612c ");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("AdeleNikitina", "java_learning")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+
+}
