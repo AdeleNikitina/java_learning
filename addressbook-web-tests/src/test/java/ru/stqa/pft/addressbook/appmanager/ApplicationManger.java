a3b56f806779454ffa1f077af074c90ca14a2631
AdeleNikitina <57428032+AdeleNikitina@users.noreply.github.com>
2020-06-19 16:40:02 +0500

    Лекция 10.2. Удалённый запуск браузера (Selenium Server)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManger.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManger.java
@@ -5,10 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -32,13 +35,19 @@ public class ApplicationManger {
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
     dbHelper = new DbHelper();
-
-    if (browser.equals(BrowserType.FIREFOX)) {
-      webDriver = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)) {
-      webDriver = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IE)) {
-      webDriver = new InternetExplorerDriver();
+    if ("".equals(properties.getProperty("selenium.server"))) {
+      if (browser.equals(BrowserType.FIREFOX)) {
+        webDriver = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)) {
+        webDriver = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+        webDriver = new InternetExplorerDriver();
+      }
+    }
+    else {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      webDriver = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
     webDriver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
     webDriver.get(properties.getProperty("web.baseUrl"));
